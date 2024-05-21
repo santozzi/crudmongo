ded3fe429f11e8f904f75817359136dcac57c335
Sergio Antozzi <santozzi@gmail.com>
2024-05-21 07:51:41 -0300

    env url y login

--- src/config/express.config.ts
+++ src/config/express.config.ts
@@ -2,11 +2,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 export interface ExpressConfig{
     PORT_EXPRESS:string;
+    URL:string;
 }
-const PORT_EXPRESS = process.env.PORT_EXPRESS;
+const {PORT_EXPRESS,URL} = process.env;
 if(!PORT_EXPRESS)
     throw new Error ("PORT fail in env")
-
+if(!URL)
+    throw new Error ("URL fail in env")
 export const expressConf:ExpressConfig={
-    PORT_EXPRESS
+    PORT_EXPRESS,
+    URL
 }
--- src/controllers/root.controller.ts
+++ src/controllers/root.controller.ts
@@ -2,16 +2,17 @@ import {Request,Response} from 'express';
 import { findAllRootModel } from '../models/product.model';
 import { findAllRootModel as findAllCat } from '../models/category.model';
 import { findAllRoot } from '../models/user.model';
-
+import { expressConf } from '../config/express.config';
+const URL = expressConf.URL;
 export async function home(req:Request, res:Response){
   const products = await findAllRootModel();
   const categories = await findAllCat();
   const users = await findAllRoot();
 
   
-  res.render("home",{products,categories,users});
+  res.render("home",{products,categories,users,URL});
 }
 
 export async function documentation(req:Request, res:Response){
-  res.render("documentation");
+  res.render("documentation",{URL});
 }
