a4ee21818d370367b6bbf198959fb29fc715fdae
Sergio Antozzi <santozzi@gmail.com>
2024-05-23 07:18:51 -0300

    routes access

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -7,9 +7,9 @@ const productRoute = Router();
 
 //endpoints
 productRoute.post("/create", verifyTokenMiddleware, create);
-productRoute.get("/", verifyTokenMiddleware, findAll);
+productRoute.get("/",  findAll);
 //por params le paso name
-productRoute.get("/:id", verifyTokenMiddleware, findById);
+productRoute.get("/:id", findById);
 productRoute.delete("/delete/:id", verifyTokenMiddleware, deleteProduct);
 productRoute.put("/update/:id", verifyTokenMiddleware, update);
 export default productRoute;
